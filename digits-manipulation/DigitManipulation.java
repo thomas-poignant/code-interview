38398faa63e6a45c31bb40a13a2b87f421b40831
Thomas Poignant <thomas.poignant@gmail.com>
2018-08-27 23:34:35 +0200

    add digit manipulation

---
+++ digits-manipulation/DigitManipulation.java
@@ -0,0 +1,78 @@
+package com.tpoi.sandbox;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DigitManipulation
+{
+  
+  public static void main(String... args)
+  {
+    biggestDigit(423865); //return 865432
+    nextDigit(423865); //return 425863
+  }
+  
+  /**
+   * find the biggest digit with all the digits in the number
+   * @param number - initial number
+   * @return the biggest number with the digits
+   */
+  public static int biggestDigit(int number)
+  {
+    String numberInString = Integer.toString(number);
+    List<Integer> digits = new ArrayList<>(numberInString.length());
+    for(int i = 0;i<numberInString.length();i++)
+    {
+      char currentDigits = numberInString.charAt(i);
+      digits.add(Integer.valueOf(String.valueOf(currentDigits)));
+    }
+  
+    Collections.sort(digits);
+    Collections.reverse(digits);
+    String resultInString = "";
+    for(Integer digit:digits)
+    {
+      resultInString+=digit;
+    }
+    return Integer.valueOf(resultInString);
+  }
+  
+  /**
+   * find the next number with the same digits
+   * @param number - initial number
+   * @return the next number with the same digits
+   */
+  public static int nextDigit(int number)
+  {
+    String numberInString = Integer.toString(number);
+    List<Integer> digits = new ArrayList<>(numberInString.length());
+    
+    digits.add(Character.getNumericValue(numberInString.charAt(numberInString.length()-1)));
+    for(int i = numberInString.length()-2;i>0;i--)
+    {
+      int lastElement = Character.getNumericValue(numberInString.charAt(i-1));
+      int currentElement = Character.getNumericValue(numberInString.charAt(i));
+      
+      digits.add(currentElement);
+      if(lastElement<currentElement)
+      {
+        digits.add(lastElement);
+        Collections.sort(digits);
+        Collections.reverse(digits);
+        
+        int elementToMove = digits.get(digits.size()-2);
+        digits.remove(digits.size()-2);
+        
+        String endOfDigit = "";
+        for(Integer digit:digits)
+        {
+          endOfDigit+=digit;
+        }
+        return Integer.valueOf(numberInString.substring(0,i-1)+elementToMove+endOfDigit);
+      }
+    }
+    return number;
+  }  
+}
+
---
+++ digits-manipulation/README.md
@@ -0,0 +1,18 @@
+# Digit Manipulation
+
+This one is from [here](http://blog.gainlo.co/index.php/2017/01/20/arrange-given-numbers-to-form-the-biggest-number-possible/).
+
+## First case 
+
+Write a function that takes a number as input and outputs the biggest number with the same set of digits.
+
+```
+For example, suppose the input is 423865, then the biggest number with these digits is 865432.
+```
+
+## Second case
+Let’s change the question a little bit. How can you get the next greater number with the same set of digits?
+
+```
+For example, suppose the input is 423862, then the biggest number with these digits is 426238.
+```
